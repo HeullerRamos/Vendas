6c2f0fe727c7c044db3fae72f3b64edf045c007a
IFNMG <IFNMG@LABCOMP1-COM-31>
2025-05-08 08:47:02 -0300

    Ta funcionando

---
+++ src/main/java/com/mycompany/registrarvendas/RegistrarVendas.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrarvendas;
+
+
+/**
+ *
+ * @author IFNMG
+ */
+public class RegistrarVendas {
+
+    public static void main(String[] args) {
+         
+//        Produto p1 = new Produto("Refri",5.0); 
+//        Produto p2 = new Produto("Arroz",18.0); 
+//        Produto p3 = new Produto("Pao",3.0); 
+//        
+//        ItemVenda it1 = new ItemVenda(8,p1);
+//        ItemVenda it2 = new ItemVenda(2,p2);
+//        ItemVenda it3 =  new ItemVenda(4,p3);
+//      
+//        
+//        Venda v1 = new Venda();
+//        v1.adicionarItens(it3);
+//        v1.adicionarItens(it2);
+//        
+//        Venda v2 = new Venda();
+//        v1.adicionarItens(it1);
+//        //v1.adicionarItens(it2);
+//        
+//        Caixa c1 = new Caixa(1L,"Josivaldo");
+//        c1.insereVendas(v2);
+//        c1.insereVendas(v1);
+//        c1.vendas.forEach(a -> {
+//        a.status=true;});
+//        
+//        for(int i = 0; i < c1.vendas.size();i++){
+//            System.out.println("Venda: "+ (i + 1) +" " + c1.vendas.get(i).status);
+//        }
+
+          Caixa caixa = new Caixa (1l,"Adalberto");
+    }
+         
+    
+    
+}
