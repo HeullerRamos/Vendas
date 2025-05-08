2cd8856253107bfc0bc9307279e3789929958353
IFNMG <IFNMG@LABCOMP1-COM-31>
2025-05-08 18:23:02 -0300

    Adequação do Controler

--- src/main/java/com/mycompany/registrarvendas/VendaControler.java
+++ src/main/java/com/mycompany/registrarvendas/VendaControler.java
@@ -1,13 +1,20 @@
 package com.mycompany.registrarvendas;
 
 public class VendaControler {
+    private Venda venda;
 
+    public VendaControler() {
+        venda = new Venda();
+    }
 
     public  Venda adicionarItensVenda(ItemVenda item) {
-        Venda venda =  new Venda();
 
         venda.adicionarItens(item);
 
+        return venda;
+    }
+
+    public Venda getVenda() {
         return venda;
     }
 }
